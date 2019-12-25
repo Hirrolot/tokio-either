759f9126606aafe52fedf307fda2180b35f04f00
Temirkhan Myrzamadi <hirrolot@gmail.com>
2019-12-25 21:00:12 +0600

    Remove Unpin

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,10 @@ use std::{
     pin::Pin,
     task::{Context, Poll},
 };
-use tokio::io::{AsyncRead, AsyncWrite};
+use tokio::{
+    io::{AsyncRead, AsyncWrite},
+    stream::Stream,
+};
 
 #[derive(Debug, Clone)]
 pub enum Either<L, R> {
@@ -12,48 +15,56 @@ pub enum Either<L, R> {
 
 impl<L, R> AsyncRead for Either<L, R>
 where
-    L: AsyncRead + Unpin,
-    R: AsyncRead + Unpin,
+    L: AsyncRead,
+    R: AsyncRead,
 {
     fn poll_read(
         self: Pin<&mut Self>,
         cx: &mut Context,
         buf: &mut [u8],
     ) -> Poll<tokio::io::Result<usize>> {
-        match self.get_mut() {
-            Self::Left(l) => Pin::new(l).poll_read(cx, buf),
-            Self::Right(r) => Pin::new(r).poll_read(cx, buf),
+        unsafe {
+            match self.get_unchecked_mut() {
+                Self::Left(l) => Pin::new_unchecked(l).poll_read(cx, buf),
+                Self::Right(r) => Pin::new_unchecked(r).poll_read(cx, buf),
+            }
         }
     }
 }
 
 impl<L, R> AsyncWrite for Either<L, R>
 where
-    L: AsyncWrite + Unpin,
-    R: AsyncWrite + Unpin,
+    L: AsyncWrite,
+    R: AsyncWrite,
 {
     fn poll_write(
         self: Pin<&mut Self>,
         cx: &mut Context,
         buf: &[u8],
     ) -> Poll<tokio::io::Result<usize>> {
-        match self.get_mut() {
-            Self::Left(l) => Pin::new(l).poll_write(cx, buf),
-            Self::Right(r) => Pin::new(r).poll_write(cx, buf),
+        unsafe {
+            match self.get_unchecked_mut() {
+                Self::Left(l) => Pin::new_unchecked(l).poll_write(cx, buf),
+                Self::Right(r) => Pin::new_unchecked(r).poll_write(cx, buf),
+            }
         }
     }
 
     fn poll_flush(self: Pin<&mut Self>, cx: &mut Context) -> Poll<tokio::io::Result<()>> {
-        match self.get_mut() {
-            Self::Left(l) => Pin::new(l).poll_flush(cx),
-            Self::Right(r) => Pin::new(r).poll_flush(cx),
+        unsafe {
+            match self.get_unchecked_mut() {
+                Self::Left(l) => Pin::new_unchecked(l).poll_flush(cx),
+                Self::Right(r) => Pin::new_unchecked(r).poll_flush(cx),
+            }
         }
     }
 
     fn poll_shutdown(self: Pin<&mut Self>, cx: &mut Context) -> Poll<tokio::io::Result<()>> {
-        match self.get_mut() {
-            Self::Left(l) => Pin::new(l).poll_shutdown(cx),
-            Self::Right(r) => Pin::new(r).poll_shutdown(cx),
+        unsafe {
+            match self.get_unchecked_mut() {
+                Self::Left(l) => Pin::new_unchecked(l).poll_shutdown(cx),
+                Self::Right(r) => Pin::new_unchecked(r).poll_shutdown(cx),
+            }
         }
     }
 }
